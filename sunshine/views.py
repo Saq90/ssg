c5ef058b0925ca06c5a10879955d8b690b8f72e3
Saquib <saquibjawed0@gmail.com>
2021-02-10 16:45:37 +0530

    initial commit

--- sunshine/views.py
+++ sunshine/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render,redirect
 from .models import Contact
+from django.contrib import messages
 
 def home(request):
     return render(request,'home.html')
@@ -11,8 +12,11 @@ def contact(request):
         name=request.POST.get('name', '')
         email=request.POST.get('email', '')
         message=request.POST.get('message', '')
-
-        contact = Contact(name=name,email=email,message=message)
-        contact.save()
-        return redirect('/')
+        if len(name)<3 or len(email)<3:
+            messages.error(request, "Please fill the form correctly")
+        else:
+           contact = Contact(name=name,email=email,message=message)
+           contact.save()
+           messages.success(request, "Your message has been successfully sent")
+           return redirect('/contact/')
     return render(request,'contact.html')
