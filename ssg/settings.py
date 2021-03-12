fb1c1ad7ffda56222490ae4a44a1c3e7fdadd2f0
Saquib <saquibjawed0@gmail.com>
2021-03-12 17:27:38 +0530

    initial commit

--- ssg/settings.py
+++ ssg/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'sunshine/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
 #
 
